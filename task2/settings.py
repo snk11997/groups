d92a8299c65fb4c09f991f19b0e212c80cdcd654
AloneAravind <kothalamaravind@gmail.com>
2024-07-16 18:19:21 +0530

    all files added

--- task2/settings.py
+++ task2/settings.py
@@ -76,12 +76,10 @@ WSGI_APPLICATION = 'task2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'task1',  # db name
-        'USER': 'root',
-        'PASSWORD': 'kothalamaravind08*',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        "NAME":BASE_DIR / 'db.sqlite3',
+
+        
     }
 }
   
